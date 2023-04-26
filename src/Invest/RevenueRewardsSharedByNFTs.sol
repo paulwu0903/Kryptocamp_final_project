07e750b18b5f96d34d3f8440d8d0c9493c51878c
Paul Wu <paulwu0903@gmail.com>
2023-04-26 21:37:32 +0800

    New: 新增分潤合約

---
+++ src/Invest/RevenueRewardsSharedByNFTs.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "../ERC721A/ITrendMasterNFT.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract RevenueRewardsSharedByNFTs is ReentrancyGuard{
+
+    ITrendMasterNFT public trendMasterNFT;
+
+    mapping (address => bool) isReceiveRewards;
+
+    constructor(address _nft) payable {
+        trendMasterNFT = ITrendMasterNFT(_nft);
+    }
+
+    function getRewards() external nonReentrant {
+        require(trendMasterNFT.balanceOf(msg.sender) != 0, "You don't have any NFTs.");
+        require(!isReceiveRewards[msg.sender], "already received!");
+        uint256 rewards = (address(this).balance / trendMasterNFT.totalSupply()) * trendMasterNFT.balanceOf(msg.sender);
+        isReceiveRewards[msg.sender] = true;
+        payable(msg.sender).transfer(rewards);
+    }
+
+}
---
+++ src/Invest/RevenueRewardsSharedByTokens.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "../ERC20/ITrendToken.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "../Stake/ITokenStakingRewards.sol";
+import "../Stake/INFTStakingRewards.sol";
+
+contract RevenueRewardsSharedByTokens is ReentrancyGuard{
+
+    ITrendToken public trendToken;
+    ITokenStakingRewards public tokenStakingRewards;
+    INFTStakingRewards public nftStakingRewards;
+
+    uint256 public snapshotId;
+
+    mapping (address => bool) isReceiveRewards;
+
+
+
+    constructor(address _token, address _tokenStaking, address _nftStaking, uint256 _snapshotId) payable{
+        trendToken = ITrendToken(_token);
+        tokenStakingRewards = ITokenStakingRewards(_tokenStaking);
+        nftStakingRewards = INFTStakingRewards(_nftStaking);
+        snapshotId = _snapshotId;
+    }
+
+    function getRewards() external nonReentrant {
+        require(trendToken.balanceOfAt(msg.sender, snapshotId) != 0, "You don't have any tokens.");
+        require(!isReceiveRewards[msg.sender], "already received!");
+        uint256 rewards = (address(this).balance / trendToken.totalSupplyAt(snapshotId) - tokenStakingRewards.getRemainTokens() - nftStakingRewards.getRemainTokens()) * trendToken.balanceOfAt(msg.sender, snapshotId);
+        isReceiveRewards[msg.sender] = true;
+        payable(msg.sender).transfer(rewards);
+    }
+
+}
