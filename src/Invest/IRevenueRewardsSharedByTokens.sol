047c3684c46bae35ce3e1cea08d376edea97128f
Paul Wu <paulwu0903@gmail.com>
2023-04-28 23:57:09 +0800

    Update: 添增分潤合約介面

---
+++ src/Invest/IRevenueRewardsSharedByTokens.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "../ERC20/ITrendToken.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "../Stake/ITokenStakingRewards.sol";
+import "../Stake/INFTStakingRewards.sol";
+
+interface IRevenueRewardsSharedByTokens{
+    function getRewards() external;
+
+}
