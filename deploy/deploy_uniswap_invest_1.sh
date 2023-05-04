06d5ff38c292446677f4fd196428d40d56be331a
Paul Wu <paulwu0903@gmail.com>
2023-05-04 22:01:55 +0800

    Update: 更改uniswapInvest腳本和trend token腳本部署順序及所需參數

--- deploy/deploy_uniswap_invest_1.sh
+++ deploy/deploy_uniswap_invest_1.sh
@@ -1,6 +1,6 @@
 forge create UniswapV2Invest \
 --rpc-url=$RPC_URL \
 --private-key=$PRIVATE_KEY \
---constructor-args $UNISWAP_CONTRACT \
+--constructor-args $UNISWAP_ROUTE_CONTRACT $UNISWAP_FACTORY_CONTRACT $TREND_TOKEN_CONTRACT \
 --verify \
 --etherscan-api-key $API_KEY
